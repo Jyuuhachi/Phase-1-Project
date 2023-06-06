dda36cf3700f6adee50223a7b8c0dc94e670f002
Avi Kaul <avikarkaul@gmail.com>
2023-06-06 16:27:36 -0400

    started working on js

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-function getJson() {
-    fetch('')
-    .then (resp => resp.json())
-    .then (data => (data.forEach(boink) => renderData(data)
-    }
+bootcampURL = "http://localhost:3000/bootcamps"
+commentsURL = "http://localhost:3000/comments"
+fetch(bootcampURL)
+.then(resp => resp.json())
+.then(bootcamps =>{bootcamps.forEach(bootcamp=>displayBootcamp(bootcamp))})
+
+function displayBootcamp(bootcamp){
+    const card = document.querySelector(".image-card")
+    card.querySelector("bootcamp-name").textContent = bootcamp.name
+    console.log(bootcamp)
+}
+
     
     
+/* function addComment(comment){}
     
-    function addComment(comment) {
+function deleteComment(comment){}
     
-    }
+function addNewComment(comment){}
     
-    function deleteComment(comment) {
+function addBootCamp(bootCamp){}
     
-    }
+function editbootCamp(bootCamp){}
     
-    function addNewComment(comment) {
     
-    }
-    
-    function addBootCamp(bootCamp) {
-    
-    }
-    
-    function edit bootCamp(bootCamp) {
-    
-    }
-    
-    function caroselBootCamps(direction) {
-    
-    }
-    
-    function updateLikes() {
-    
-    }
+function updateLikes(){}
+ */
