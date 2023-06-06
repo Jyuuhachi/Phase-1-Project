ccee94be3881aa0bd3a69efb1a681acf7c8b02d1
hellocodingclass <132718779+hellocodingclass@users.noreply.github.com>
2023-06-06 15:38:09 -0400

    added funcs to js

---
+++ index.js
@@ -0,0 +1,35 @@
+function getJson() {
+    fetch('')
+    .then (resp => resp.json())
+    .then (data => (data.forEach(boink) => renderData(data)
+    }
+    
+    
+    
+    function addComment(comment) {
+    
+    }
+    
+    function deleteComment(comment) {
+    
+    }
+    
+    function addNewComment(comment) {
+    
+    }
+    
+    function addBootCamp(bootCamp) {
+    
+    }
+    
+    function edit bootCamp(bootCamp) {
+    
+    }
+    
+    function caroselBootCamps(direction) {
+    
+    }
+    
+    function updateLikes() {
+    
+    }
