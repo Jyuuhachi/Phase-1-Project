3d0c18c07157ad3ea60e951799a9c79fb0866866
Avi Kaul <avikarkaul@gmail.com>
2023-06-07 13:17:10 -0400

    added render and update likes

--- index.js
+++ index.js
@@ -10,16 +10,70 @@ function displayBootcamp(bootcamp){
     console.log(bootcamp)
 }
     
-/* function addComment(comment){}
+/* function addComment(comment){
+}
     
-function deleteComment(comment){}
+function deleteComment(comment){
+}
     
 function addNewComment(comment){}
     
 function addBootCamp(bootCamp){}
     
-function editbootCamp(bootCamp){}
-    
-    
-function updateLikes(){}
+function editbootCamp(bootCamp){
+    const editButton = document.querySelector("#edit-button")
+    editButton.addEventListener
+}
+
+    const likeButton = document.querySelector("#likes-button")
+    likeButton.addEventListener("click", (e)=>{
+        let numLikes = card.querySelector("#likes-count").textContent
+        numberLike = numLikes +1 
+        card.querySelector("#likes-count").textContent = `${numberLike} likes`
+
+    })
+    render is dom
+    update is database
+    local and patch
+
+function updateLikes(){ 
+    const button = document.querySelector("#likes-button")
+    button.addEventListener("click", renderLikes(e))
+    const postLikes = {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+            "Accept": "application/json",
+        }
+        body: JSON.stringify({
+            bootcampList[bootcamplistPosiition].likes:"",
+        }),
+    };
+    fetch(URL, postLikes)
+    .then(likeResponse=>likeResponse.json())
+    .catch(function(error){
+        alert("Bad things!");
+        console.log(error.message)
+    })
+}
+function renderLikes(e){
+        let numLikes = card.querySelector("#likes-count").textContent
+        let numberLike = parseInt(numLikes) + 1
+        card.querySelector("#likes-count").textContent = `${numberLike} likes`
+}
+bootcampList[bootcamplistPosiition].id
  */
+
+
+
+
+
+
+function editbootCamp(bootCamp){
+    const editButton = document.querySelector("#edit-button")
+    editButton.addEventListener("submit",(edit)=>{
+        edit.preventDefault()
+
+
+    })
+}
