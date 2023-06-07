c28aefdfd07a58964896eadfca8e76e48890ac14
Christopher <qaetre@gmail.com>
2023-06-07 12:13:00 -0400

    fleshed out carousel function and updated HTML for ease of use with carousel functions

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ function getJson() {
     .then (resp => resp.json())
     .then (data => (data.forEach(boink) => renderData(data)
     }
-    
+const bootcampList = []
+let bootcampListPosition = 0
+fetch(URL)
+.then(response => response.json())
+.then(data => {
+    bootcampList = data.bootcamps
+    const bootcampImage = bootcampList[0].image
+    const bootcampDisplay = document.getElementById("image")
+    bootcampDisplay["src"] = bootcampImage
+    bootcampComments = bootcampList[0].comments
+    bootcampComments.forEach(addComment)
+
+
+})
+const leftArrow = document.getElementById("arrow-left")
+const rightArrow = document.getElementById("arrow-right")
+leftArrow.addEventListener("click", e => carouselBootcamp("left"))
+rightArrow.addEventListener("click", e => carouselBootcamp("right"))
     
     
     function addComment(comment) {
@@ -27,9 +44,79 @@ function getJson() {
     }
     
     function carouselBootcamps(direction) {
-    
+        if (direction === "left") {
+            if (bootcampListPosition === 0) {
+                bootcampListPosition = (bootcampList.length - 1)
+                const bootcampLogo = document.getElementById("image")
+                bootcampLogo['src'] = bootcampList[bootcampListPosition].image
+                const comments = carouselGetComments()
+                const likes = bootcampList[bootcampListPosition].likes
+                renderLikes(likes)
+                const commentList = document.getElementById("comment-list")
+                while (commentList.firstChild) {
+                    commentList.removeChild(commentList.firstChild)
+                }
+                comments.forEach(addComment)
+                const name = document.getElementById("bootcamp-name")
+                name.textContent = bootcampList[bootcampListPosition].name               
+            }
+            else {
+                bootcampListPosition -= 1
+                const bootcampLogo = document.getElementById("image")
+                bootcampLogo['src'] = bootcampList[bootcampListPosition].image
+                const comments = carouselGetComments()
+                const likes = bootcampList[bootcampListPosition].likes
+                renderLikes(likes)
+                const commentList = document.getElementById("comment-list")
+                while (commentList.firstChild) {
+                    commentList.removeChild(commentList.firstChild)
+                }
+                comments.forEach(addComment)
+                const name = document.getElementById("bootcamp-name")
+                name.textContent = bootcampList[bootcampListPosition].name
+            }
+        }
+        if (direction === "right") {
+            if (bootcampListPosition === (bootcampList.length - 1)) {
+                bootcampListPosition = 0
+                const bootcampLogo = document.getElementById("image")
+                bootcampLogo['src'] = bootcampList[bootcampListPosition].image
+                const comments = carouselGetComments()
+                const likes = bootcampList[bootcampListPosition].likes
+                renderLikes(likes)
+                const commentList = document.getElementById("comment-list")
+                while (commentList.firstChild) {
+                    commentList.removeChild(commentList.firstChild)
+                }
+                comments.forEach(addComment)
+                const name = document.getElementById("bootcamp-name")
+                name.textContent = bootcampList[bootcampListPosition].name
+            }
+            else {
+                bootcampListPosition += 1
+                const bootcampLogo = document.getElementById("image")
+                bootcampLogo['src'] = bootcampList[bootcampListPosition].image
+                const comments = carouselGetComments()
+                const likes = bootcampList[bootcampListPosition].likes
+                renderLikes(likes)
+                const commentList = document.getElementById("comment-list")
+                while (commentList.firstChild) {
+                    commentList.removeChild(commentList.firstChild)
+                }
+                comments.forEach(addComment)
+                const name = document.getElementById("bootcamp-name")
+                name.textContent = bootcampList[bootcampListPosition].name
+            }
+        }
     }
     
     function updateLikes() {
     
+    }
+    function carouselGetComments() {
+        const commentList = []
+        for (i = 0; i <= bootcampList[bootcampListPosition]['comments'].length; i++) {
+            commentList.push(bootcampList[bootcampListPosition]['comments'][i]['content'])
+        }
+        return commentList
     }
